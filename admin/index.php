3575def5b58b55925797b1ffc0d740988da614a2
xiongkequan <824178949@qq.com>
2018-01-11 22:54:11 +0800

    导航修改

--- admin/index.php
+++ admin/index.php
@@ -54,7 +54,7 @@ function menu()
 	//$sub[]  = array('url' => 'info.php?cat=contact', 	'name' => '联系我们');
 	//$sub[]  = array('url' => 'message.php', 			'name' => '留言管理');
 	$sub[]  = array('url' => 'log.php', 				'name' => '日志管理');
-	$sub[]  = array('url' => 'admin.php', 				'name' => '管理员管理');
+	$sub[]  = array('url' => 'admin.php', 				'name' => '管理员设置');
 	$sub[]  = array('url' => 'member.php', 				'name' => '会员列表');
 	$sub[]  = array('url' => 'vote.php', 	'name' => '投票管理');
 	$sub[]  = array('url' => 'news_category.php', 	'name' => '新闻分类');
