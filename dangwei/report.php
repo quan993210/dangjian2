3a89af827bb4b1a13e3b61f73528f1417f86535c
xiongkequan <824178949@qq.com>
2018-01-29 22:43:04 +0800

    更新后台

--- dangwei/report.php
+++ dangwei/report.php
@@ -293,9 +293,9 @@ function update_report()
 function export(){
     //$cashflow_db = pc_base::load_model('cashflow_model');
     print_r(1111);
-    require_once $_SERVER['DOCUMENT_ROOT'].'/inc/plugin/PHPExcel/PHPExcel.php';
-    require_once $_SERVER['DOCUMENT_ROOT'].'/inc/plugin/PHPExcel/PHPExcel/IOFactory.php';
-    require_once $_SERVER['DOCUMENT_ROOT'].'/inc/plugin/PHPExcel/PHPExcel/Reader/Excel5.php';
+    require_once '../inc/plugin/PHPExcel/PHPExcel.php';
+    require_once '../inc/plugin/PHPExcel/PHPExcel/IOFactory.php';
+    require_once '../inc/plugin/PHPExcel/PHPExcel/Reader/Excel5.php';
     print_r(222);
     global $db, $smarty;
     $where = "where status = 1";
